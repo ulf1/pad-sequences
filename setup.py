da55d97caa8ca1a59f5e1a7cbdba001be71b03fe
UH <554c46@gmail.com>
2021-04-08 10:54:33 +0200

    rst for pypi

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 from setuptools import setup
-
-
-def read(fname):
-    import os
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+import pypandoc
 
 
 setup(name='pad-sequences',
       version='0.5.0',
       description='pad variable length sequences with multiples features',
-      long_description=read('README.md'),
-      long_description_content_type='text/markdown',
+      long_description=pypandoc.convert('README.md', 'rst'),
       url='http://github.com/ulf1/pad-sequences-multi',
       author='Ulf Hamster',
       author_email='554c46@gmail.com',
